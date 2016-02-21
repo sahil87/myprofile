d8ef148e03864cd4c9fd217ecf07b84481235919
Sahil Ahuja <sahilahuja@gmail.com>
2016-02-22 00:59:48 +0530

    Modifying bashrc

--- addToBashProfile.sh
+++ addToBashProfile.sh
@@ -1,5 +1,5 @@
 file1=~/.bashrc
-if [ -f "$file1" ] && grep "mybash_profile.sh" "$file1"
+if [ -f $file1 ] && grep mybash_profile.sh $file1
   then echo "bashrc all good."
-  else echo source `pwd`/mybash_profile.sh >> "$file1"
+  else echo "source `pwd`/mybash_profile.sh" >> $file1
 fi
