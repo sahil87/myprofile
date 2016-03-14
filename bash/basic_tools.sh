bef6c2b813611347761d9d2b611a1468056d13d4
Sahil Ahuja <sahilahuja@gmail.com>
2016-03-14 09:07:04 +0530

    Removing progs

--- bash/basic_tools.sh
+++ bash/basic_tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y emacs24-nox emacs
+sudo apt-get install -y byobu emacs24-nox emacs
 sudo apt-get install -y curl wget nmap
 sudo apt-get install -y git gitk
-sudo apt-get install -y byobu
-sudo apt-get install -y cryptkeeper encfs
+sudo apt-get install -y encfs
