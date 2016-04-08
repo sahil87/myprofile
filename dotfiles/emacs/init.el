d95ac44d275b14cba27ca8fcacb633335ceab383
Sahil Ahuja <sahilahuja@gmail.com>
2016-04-08 23:46:47 +0530

    Pushing latest myprofile changes

---
+++ dotfiles/emacs/init.el
@@ -0,0 +1,8 @@
+;;In ~/.emacs.d/init.el or ~/.emacs (Using the latter currently)
+
+(require 'package)
+;;(add-to-list 'package-archives
+;;	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+		("marmalade" . "http://marmalade-repo.org/packages/")
+        ("melpa" .     "http://stable.melpa.org/packages/")))
