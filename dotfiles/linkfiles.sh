1dcaf65bceaef33a899ba0e712c6eafe7f4d4716
Sahil Ahuja <sahilahuja@gmail.com>
2016-04-10 03:06:28 +0530

    Pushing latest myprofile changes

--- dotfiles/linkfiles.sh
+++ dotfiles/linkfiles.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
+#!/bin/zsh
 
 function linkfile() {
-    echo $1 $2
+    local SOURCE=$1
+    local DEST=$2
+    local SUDO=$3
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    if (( $SUDO ))
+    then
+        sudo rm $DEST
+        sudo ln -s $DIR/$SOURCE $DEST
+    else
+        rm $DEST
+        ln -s $DIR/$SOURCE $DEST
+    fi
+
+
 }
 
-
-linkfile
+linkfile emacs/init.el          ~/.emacs                            0
+linkfile ssh/config             ~/.ssh/config                       0
+linkfile ansible/ansible.cfg    ~/.ansible.cfg                      0
+linkfile resolvconf/head        /etc/resolvconf/resolv.conf.d/head  1
+sudo resolvconf -u;
--- shell/basic_tools.sh
+++ shell/basic_tools.sh
@@ -3,12 +3,10 @@ sudo apt-get install -y byobu emacs24-nox emacs zsh zsh-doc
 sudo apt-get install -y curl wget nmap whois
 udo apt-get install -y git gitk
 sudo apt-get install -y encfs
+
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/sahil87/oh-my-zsh/master/tools/install.sh)"
-#Install magit (git for emacs)
-mkdir -p ~/.emacs.d/site-lisp
-git clone https://github.com/magit/magit.git ~/.emacs.d/site-lisp/magit
-CURRENTDIR=`pwd`
-cd ~/.emacs.d/site-lisp/magit
-make
-cd $CURRENTDIR
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm ~/.zshrc
+source $DIR/add_to_zshrc.sh
+chsh -s /bin/zsh
