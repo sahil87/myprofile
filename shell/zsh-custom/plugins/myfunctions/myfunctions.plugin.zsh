c61b20667512622fe3a7e75509f2ad6f994e1b03
Sahil Ahuja <sahilahuja@gmail.com>
2016-04-20 23:27:52 +0530

    Pushing latest myprofile changes

--- shell/zsh-custom/plugins/myfunctions/myfunctions.plugin.zsh
+++ shell/zsh-custom/plugins/myfunctions/myfunctions.plugin.zsh
@@ -1,23 +1,23 @@
 #Functions
 extract () {
-  if [ -f $1 ] ; then
-      case $1 in
-          *.tar.bz2)   tar xvjf $1    ;;
-          *.tar.gz)    tar xvzf $1    ;;
-          *.bz2)       bunzip2 $1     ;;
-          *.rar)       rar x $1       ;;
-          *.gz)        gunzip $1      ;;
-          *.tar)       tar xvf $1     ;;
-          *.tbz2)      tar xvjf $1    ;;
-          *.tgz)       tar xvzf $1    ;;
-          *.zip)       unzip $1       ;;
-          *.Z)         uncompress $1  ;;
-          *.7z)        7z x $1        ;;
-          *)           echo "don't know how to extract '$1'..." ;;
-      esac
-  else
-      echo "'$1' is not a valid file!"
-  fi
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1    ;;
+            *.tar.gz)    tar xvzf $1    ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       rar x $1       ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xvf $1     ;;
+            *.tbz2)      tar xvjf $1    ;;
+            *.tgz)       tar xvzf $1    ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "don't know how to extract '$1'..." ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
 }
 
 function swap() { # Swap 2 filenames around, if they exist (from Uzi's bashrc).
@@ -33,9 +33,17 @@ function swap() { # Swap 2 filenames around, if they exist (from Uzi's bashrc).
 }
 
 mkcd() {
-        if [ $# != 1 ]; then
-                echo "Usage: mkcd <dir>"
-        else
-                mkdir -p $1 && cd $1
-        fi
+    if [ $# != 1 ]; then
+        echo "Usage: mkcd <dir>"
+    else
+        mkdir -p $1 && cd $1
+    fi
+}
+
+ds() {
+    local depth=1;
+    if [ $# = 1 ]; then
+        depth=$1;
+    fi
+    du -hx --max-depth=$depth | sort -rh
 }
