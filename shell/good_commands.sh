548cb17e5f989dba6060e95cb0dec3d20f1fe8e5
Sahil Ahuja <sahilahuja@gmail.com>
2016-04-20 12:23:38 +0530

    Pushing latest myprofile changes

--- shell/good_commands.sh
+++ shell/good_commands.sh
@@ -15,5 +15,23 @@ screen
 ^a2 -> tab number
 ^aShift -> number
 
+lsb_release -a # system information
 
+stat fileName # file information (inode information)
+df -i # file system inode info
+df -h # file system size info
 
+free -m #Mbs free -g for gbs
+free_mem #From anshu's repo
+
+top: ShiftF (gives options to sort)
+
+lsof #List open files
+
+kill -s HUP PID #(sending SIGHUP) -> reloads without restarting
+
+iotop #Top for input/output
+iftop #Top for networking
+netstat #No of open connections sudo netstat -planet | grep 443
+tcpdump
+sar
