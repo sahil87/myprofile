9ebd21db0fd31d84f95e67fdb4848f28bcf4f092
Sahil Ahuja <sahilahuja@gmail.com>
2016-05-04 12:30:22 +0530

    Pushing latest myprofile changes

--- shell/basic_tools.sh
+++ shell/basic_tools.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y byobu emacs24-nox emacs zsh zsh-doc
 sudo apt-get install -y curl wget nmap whois
 sudo apt-get install -y git gitk
 sudo apt-get install -y encfs
+sudo apt-get install -y android-tools-adb android-tools-fastboot
 
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/sahil87/oh-my-zsh/master/tools/install.sh)"
