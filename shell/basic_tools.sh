b2ea0e0a075ce8201d632ddb1ac216021291ba6d
Sahil Ahuja <sahilahuja@gmail.com>
2016-05-11 20:31:15 +0530

    Pushing latest myprofile changes

--- shell/basic_tools.sh
+++ shell/basic_tools.sh
@@ -5,6 +5,10 @@ sudo apt-get install -y git gitk
 sudo apt-get install -y encfs
 sudo apt-get install -y android-tools-adb android-tools-fastboot
 
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/sahil87/oh-my-zsh/master/tools/install.sh)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
