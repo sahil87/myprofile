d0667114755011452c0ca5a81d52b89dec18db38
Sahil Ahuja <sahilahuja@gmail.com>
2016-05-26 10:15:30 +0530

    Pushing latest myprofile changes

--- shell/good_commands.sh
+++ shell/good_commands.sh
@@ -11,9 +11,33 @@ tar -czfv - folderName/ | ssh user@hostname 'cd whereIwantToUnzip && tar -xzfv -
 #http://blog.trackets.com/2014/05/17/ssh-tunnel-local-and-remote-port-forwarding-explained-with-examples.html
 #Reverse ssh tunnel to use a port remotely (4000) to forward all request to localhost:4280
 #So you can run server locally and server it via a public server
-ssh react-stage01.web.int.sg.aws.sportscafe.in -R 4000:localhost:4280
+ssh -R 4000:localhost:4280 react-stage01.web.int.sg.aws.sportscafe.in
 ssh -nNT react-stage01.web.int.sg.aws.sportscafe.in -R 4000:localhost:4280 #To create only tunnel not an interactive shell
 #In order to bind port to 0.0.0.0 and not 127.0.0.1 need to add "GatewayPorts yes" to /etc/ssh/sshd_config
+
+#Remote port forwarding/"Inbound tunneling"
+#Expose local services to remote clients over your outgoing cunnection
+#To share WIP web app without deploying
+local="localhost:4567"
+remote="*:8080" # "*" for all interfaces (default=loopback only)
+ssh -R ${remote}:${local} sshtalk.in
+
+#"Local" port forwarding/"Outbound tunneling"
+#Use remote service through local ports
+ssh -L ${local}:${remote} sshtalk.in
+
+#Dynamic port forwarding (Easy VPN)
+ssh -fNn -D 5555 bastion
+curl --socks5-hostname localhost:5555 -I rubygems #This runs the command on the server bastion
+
+#X11 forwarding
+ssh -XCYP hostname
+
+#Run a remote command
+ssh my-server.com -- hotsname
+
+#Agent forwardings
+ssh -A my-server.com #Will forward my private key in the session. Remote needs to be trustable, or private key can be compromized.
 
 #To check ports bound in local system
 netstat -tunlp
