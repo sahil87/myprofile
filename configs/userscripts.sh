0736c6822a6c9d7854f244255b736c945f65f6b0
Sahil Ahuja <sahilahuja@gmail.com>
2016-06-03 22:24:08 +0530

    Pushing latest myprofile changes

--- configs/userscripts.sh
+++ configs/userscripts.sh
@@ -1,5 +1,6 @@
 #To create a new user:
 #user add -G sudo -d /new/hom/dir -s /bin/bash
 useradd -s /bin/bash sirajul
+adduser sirajul #Adds with template
 #Add user to groups
 usermod -a -G sudo,docker sirajul
