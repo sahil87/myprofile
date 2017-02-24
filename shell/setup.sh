60e117d5fa0605d65a4644452ec7420dec243dc5
Sahil Ahuja <sahilahuja@gmail.com>
2017-02-24 15:41:57 +0530

    Pushing latest myprofile changes

--- shell/setup.sh
+++ shell/setup.sh
@@ -9,6 +9,7 @@ sudo apt install -y rygel rygel-preferences
 #For UI:
 sudo apt install -y yakuake xcalib
 
+sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt install -y oracle-java8-installer
