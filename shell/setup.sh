64613c06e50087d5f4eb7aedacd2e9dcb3138654
Sahil Ahuja <sahilahuja@gmail.com>
2017-03-04 12:39:51 +0530

    Docker-ce install

--- shell/setup.sh
+++ shell/setup.sh
@@ -62,7 +62,7 @@ sudo apt install -y nodejs build-essential
 #Install docker
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(grep UBUNTU_CODENAME /etc/os-release | awk -F '=' '{ print $2 }') stable edge"
 sudo apt-get update
 sudo apt-get -y install docker-ce
 sudo service docker stop
