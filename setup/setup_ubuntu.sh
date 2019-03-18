7eaf2f0cd09d445bb27d952ddf0553261cbf9f8b
Sahil Ahuja <sahilahuja@gmail.com>
2019-03-18 06:39:34 +0530

    Pushing latest profile/blog changes

--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#Link files
-cd dotfiles; ./linkfiles_ubuntu.sh; cd ..;
-
 #Important tools:
 #Install chrome, vscode, vivaldi
 sudo apt install -y emacs zsh zsh-doc git gitk tig yakuake hplip-gui  #emacs24-nox xcalib byobu
 sudo apt install -y curl wget nmap whois encfs openssh-server gparted #luckybackup
 sudo apt install -y autoconf automake libtool cmake
 #sudo apt install -y android-tools-adb android-tools-fastboot #rygel rygel-preferences
+
+#Link files
+cd dotfiles; ./linkfiles_ubuntu.sh; cd ..;
 
 #Install oh-my-zsh (Run from shell folder)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/sahil87/oh-my-zsh/master/tools/install.sh)"
