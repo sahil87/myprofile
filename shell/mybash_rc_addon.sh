ffd21e8bf4d35a47276a77ad4bf082c18235f231
Sahil Ahuja <sahilahuja@gmail.com>
2020-05-31 13:04:40 +0530

    Pushing latest profile/blog changes

--- shell/mybash_rc_addon.sh
+++ shell/mybash_rc_addon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Used only as an addon to the dec mybashrc
+#Used only as an addon to the dec mybashrc (in case mybash_rc.sh isn't used)
 export EDITOR=emacs
 CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export DIR87=`realpath $CURRENTDIR/../..`
@@ -7,6 +7,7 @@ export MYPROFILEDIR=`realpath $DIR87/myprofile`
 export LIFETRACKERDIR=`realpath $DIR87/lifetracker`
 export BLOGDIR=`realpath $DIR87/bloghexo`
 export PATH=$PATH:$MYPROFILEDIR/bin
+alias c='cd ~/code/'
 alias vr='cd ~/code/gmetrivr/'
 alias 87='cd ~/code/sahil87/'
 export AWS_CONFIG_FILE=~/.aws/config
@@ -16,7 +17,7 @@ alias ssh-add-personal="source $MYPROFILEDIR/bin/ssh-add-personal-script"
 alias ssh-add-work="source $MYPROFILEDIR/bin/ssh-add-work-script"
 
 #Lines in ~/.bashrc:
-#source /home/gmetri/code/gmetrivr/dec/shell/mybash_rc.sh #v1.1
-#source /home/gmetri/code/gmetrivr/dec/shell/mybash_theme.sh #v1.1
-#source /home/gmetri/code/gmetrivr/dec/shell/mybash_vars.sh #v1.1
+#source /home/gmetri/code/dec/shell/mybash_rc.sh #v1.1
+#source /home/gmetri/code/dec/shell/mybash_theme.sh #v1.1
+#source /home/gmetri/code/dec/shell/mybash_vars.sh #v1.1
 #source /home/gmetri/code/sahil87/myprofile/shell/mybash_rc_addon.sh
