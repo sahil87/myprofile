01936d4687b0e2be48cd388832861f99c6fdf3af
Sahil Ahuja <sahilahuja@gmail.com>
2020-08-05 14:08:04 +0530

    Pushing latest profile/blog changes

--- dotfiles/linkfiles_ubuntu.sh
+++ dotfiles/linkfiles_ubuntu.sh
@@ -19,8 +19,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/dotfiles
 linkfile $CURRENT_DIR/ssh/ssh.config             ~/.ssh/config                           0
 linkfile $CURRENT_DIR/git/.gitconfig             ~/.gitconfig                            0
 linkfile $CURRENT_DIR/sysctl/40-max-user-watches.conf /etc/sysctl.d/40-max-user-watches.conf       1
-linkfile $CURRENT_DIR/imwheel/imwheelrc          ~/.imwheelrc                            0
-linkfile $CURRENT_DIR/imwheel/Imwheels.desktop   ~/.config/autostart/Imsheels.desktop    0
+#linkfile $CURRENT_DIR/imwheel/imwheelrc          ~/.imwheelrc                            0
+#linkfile $CURRENT_DIR/imwheel/Imwheels.desktop   ~/.config/autostart/Imsheels.desktop    0
 #linkfile custom-apps-menu/applications-override.json  ~/.local/share/cinnamon/applets/custom-apps-menu@sahil87/applications-override.json                        0
 #linkfile $CURRENT_DIR/ansible/.ansible.cfg       ~/.ansible.cfg                          0
 #linkfile $CURRENT_DIR/tmux.conf                  ~/.tmux.conf                            0
--- shell/setup/add_to_zshrc.sh
+++ shell/setup/add_to_zshrc.sh
@@ -2,12 +2,12 @@
 
 RC_FILE=~/.zshrc
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARENT_DIR=`realpath $CURRENT_DIR/../..`
+#PARENT_DIR=`realpath $CURRENT_DIR/../..`
 
 if [ -f $RC_FILE ] && grep "myzsh_rc.sh #v1.1" $RC_FILE
   then echo "zrc all good."
   else
     set -x
     sed -i '/^source \$ZSH\/oh-my-zsh.sh/s/^/# /' $RC_FILE
-    echo "source $PARENT_DIR/shell/myzsh_rc.sh #v1.1" >> $RC_FILE
+    echo "source $CURRENT_DIR/shell/myzsh_rc.sh #v1.1" >> $RC_FILE
 fi
