28681fe25059e985537842421baa705794b48723
Sahil Ahuja <sahilahuja@gmail.com>
2021-05-15 10:11:41 +0000

    Pushing latest profile/blog changes

--- shell/mybash_rc_addon.sh
+++ shell/mybash_rc_addon.sh
@@ -12,7 +12,7 @@ export PATH=$PATH:$MYPROFILEDIR/bin
 # alias 87='cd ~/code/sahil87/'
 #export AWS_CONFIG_FILE=~/.aws/config #this is the default
 source $MYPROFILEDIR/shell/custom-bash/myaliases.sh
-source ~/code/dec/kubeconfig/env.sh
+source ~/code/kube/cc/kubeconfig/env.sh
 alias ssh-add-personal="source $MYPROFILEDIR/bin/ssh-add-personal-script"
 alias ssh-add-work="source $MYPROFILEDIR/bin/ssh-add-work-script"
 
