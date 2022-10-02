e771d23813ebccdfd2245e1089b77d9855699dce
Sahil Ahuja <sahilahuja@gmail.com>
2022-10-02 14:08:35 +0530

    Macos changes

--- shell/myzsh_rc_macos.sh
+++ shell/myzsh_rc_macos.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-path=($path ß$CODEDIR/gmetrivr/dec/macos-bin)
+CURRENTDIR=${0:a:h}
+export MYPROFILEDIR=`realpath $CURRENTDIR/..`
+export DIR87=`realpath $MYPROFILEDIR/..`
+export CODEDIR=`realpath $MYPROFILEDIR/../..`
+
+#First match wins. So the path that comes earlier has higher precedence
+path=($path $MYPROFILEDIR/bin-macos $CODEDIR/dec/shell/bin-macos)
